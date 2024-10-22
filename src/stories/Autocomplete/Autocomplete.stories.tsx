92b5eec21fb68fd9777ad89ac6acbe7fcbb1b94d
Javier Ponce <imr3ivaj@gmail.com>
2024-10-22 13:08:00 -0600

    chore: remove unneeded comments

--- src/stories/Autocomplete/Autocomplete.stories.tsx
+++ src/stories/Autocomplete/Autocomplete.stories.tsx
@@ -1,6 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
-// import { fn } from '@storybook/test';
 import { UserCircleIcon } from "../assets/icons";
 
 import { Autocomplete } from "./Autocomplete.component";
@@ -20,12 +18,6 @@ const meta = {
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   backgroundColor: { control: 'color' },
-  // },
-  // // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  // args: { onClick: fn() },
 } satisfies Meta<typeof Autocomplete>;
 
 export default meta;
